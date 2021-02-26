9e09d4448d9c67d10bb6548a0d4644c10ede69a5
NejcZdovc <nejc.zdovc@3zsistemi.si>
2021-02-26 17:18:10 +0100

    Adds global js

--- app/root.tsx
+++ app/root.tsx
@@ -49,6 +49,7 @@ export default function App() {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.png' />
+        <script type='application/javascript' src='/global.js' />
         <Meta />
         <Links />
         <Scripts />
