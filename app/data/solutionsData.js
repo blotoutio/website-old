1fa0e60bfef739965cf458742a8e75ed96801ea5
Yash Ghelani <contact@yashghelani.com>
2022-01-26 00:39:11 +0000

    Add test data structure for solutions pages

---
+++ app/data/solutionsData.js
@@ -0,0 +1,57 @@
+export const solutionsData = [
+  {
+    type: 'main',
+    persona: 'engineer',
+    features: [
+      {
+        type: 'feature',
+        title: 'EL + T',
+        slug: 'el-plus-t',
+        feature_title: 'EL + T',
+        feature_slug: 'el-plus-t',
+        slides: [
+          {
+            type: 'slide',
+            number: '0',
+            title: 'Some text here',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+            slide_title: 'Some text here',
+            slide_description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+          },
+          {
+            type: 'slide',
+            number: '1',
+            title: 'Step 1: Source',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+            slide_title: 'Step 1: Source',
+            slide_description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+          },
+          {
+            type: 'slide',
+            number: '2',
+            title: 'Step 2: Schema',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+            slide_title: 'Step 2: Schema',
+            slide_description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+          },
+          {
+            type: 'slide',
+            number: '3',
+            title: 'Step 3: Summary',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+            slide_title: 'Step 3: Summary',
+            slide_description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin libero eget tincidunt. Donec sit amet aliquam risus.',
+          },
+        ],
+      },
+    ],
+  },
+]
