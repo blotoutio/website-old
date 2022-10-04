95fab1dc76d6a448a740378445921c56fbffe906
Yash Ghelani <75333858+yashghelani@users.noreply.github.com>
2022-10-04 12:07:32 +0000

    Add animated client logo slider

--- app/components/home/HeroSection/index.jsx
+++ app/components/home/HeroSection/index.jsx
@@ -37,44 +37,86 @@ const HeroSection = () => {
 
       <div id='customers'>
         <div id='customers-text'>Trusted by leading brands and agencies</div>
-        <div id='customers-list'>
-          <img src='img/core/customer/razer.svg' alt='Logo of Razer' />
-          <img src='img/core/customer/uphold.svg' alt='Logo of Uphold' />
-          <img
-            src='img/core/customer/honda.svg'
-            alt='Logo of Honda'
-            width={100}
-          />
-          <img
-            src='img/core/customer/globalid.svg'
-            alt='Logo of GlobaliD'
-            width={100}
-          />
-          <img
-            src='img/core/customer/ethos_life.svg'
-            alt='Logo of Ethos Life'
-            width={100}
-          />
-          <img
-            src='img/core/customer/consumer_cellular.svg'
-            alt='Logo of Consumer Cellular'
-            width={125}
-          />
-          <img
-            src='img/core/customer/fubo_tv.svg'
-            alt='Logo of Fubo TV'
-            width={75}
-          />
-          <img
-            src='img/core/customer/pura_vida.svg'
-            alt='Logo of Pura Vida'
-            width={100}
-          />
-          <img
-            src='img/core/customer/loop_club.svg'
-            alt='Logo of Loop Club'
-            width={40}
-          />
+        <div id='customers-list-outer-container'>
+          <div id='customers-list-inner-container'>
+            <img src='img/core/customer/razer.svg' alt='Logo of Razer' />
+            <img src='img/core/customer/uphold.svg' alt='Logo of Uphold' />
+            <img
+              src='img/core/customer/honda.svg'
+              alt='Logo of Honda'
+              width={100}
+            />
+            <img
+              src='img/core/customer/globalid.svg'
+              alt='Logo of GlobaliD'
+              width={100}
+            />
+            <img
+              src='img/core/customer/ethos_life.svg'
+              alt='Logo of Ethos Life'
+              width={100}
+            />
+            <img
+              src='img/core/customer/consumer_cellular.svg'
+              alt='Logo of Consumer Cellular'
+              width={125}
+            />
+            <img
+              src='img/core/customer/fubo_tv.svg'
+              alt='Logo of Fubo TV'
+              width={75}
+            />
+            <img
+              src='img/core/customer/pura_vida.svg'
+              alt='Logo of Pura Vida'
+              width={100}
+            />
+            <img
+              src='img/core/customer/loop_club.svg'
+              alt='Logo of Loop Club'
+              width={40}
+            />
+
+            {/* Get rid of this copy by using a map function */}
+
+            <img src='img/core/customer/razer.svg' alt='Logo of Razer' />
+            <img src='img/core/customer/uphold.svg' alt='Logo of Uphold' />
+            <img
+              src='img/core/customer/honda.svg'
+              alt='Logo of Honda'
+              width={100}
+            />
+            <img
+              src='img/core/customer/globalid.svg'
+              alt='Logo of GlobaliD'
+              width={100}
+            />
+            <img
+              src='img/core/customer/ethos_life.svg'
+              alt='Logo of Ethos Life'
+              width={100}
+            />
+            <img
+              src='img/core/customer/consumer_cellular.svg'
+              alt='Logo of Consumer Cellular'
+              width={125}
+            />
+            <img
+              src='img/core/customer/fubo_tv.svg'
+              alt='Logo of Fubo TV'
+              width={75}
+            />
+            <img
+              src='img/core/customer/pura_vida.svg'
+              alt='Logo of Pura Vida'
+              width={100}
+            />
+            <img
+              src='img/core/customer/loop_club.svg'
+              alt='Logo of Loop Club'
+              width={40}
+            />
+          </div>
         </div>
       </div>
     </Section>
