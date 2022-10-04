669177d5f8071005aeb3eb237159c972fc0e1494
Yash Ghelani <75333858+yashghelani@users.noreply.github.com>
2022-10-04 09:25:40 +0000

    Add explainer illustrations with toggle button

--- app/components/home/MainSection/index.jsx
+++ app/components/home/MainSection/index.jsx
@@ -1,13 +1,42 @@
+import { useState } from 'react'
 import Section from '~/components/core/Section'
 
 const MainSection = (props) => {
   const { heading, sub_heading } = props
+
+  const [toggled, setToggled] = useState(true)
+
+  const toggleImage = () => setToggled(!toggled)
 
   return (
     <Section id='main-section'>
       <div id='main-container'>
         <h2>{heading}</h2>
         <p>{sub_heading}</p>
+        <div id='graphic'>
+          <div id='toggle-container'>
+            <div
+              id='without-switch'
+              className={'switch ' + (toggled ? '' : 'selected')}
+              onClick={() => toggleImage()}
+            >
+              Without Blotout
+            </div>
+            <div
+              id='with-switch'
+              className={'switch ' + (toggled ? 'selected' : '')}
+              onClick={() => toggleImage()}
+            >
+              With Blotout
+            </div>
+          </div>
+          <div id='image-container'>
+            <img
+              src={`img/home/${toggled ? 'with' : 'without'}-blotout.svg`}
+              alt=''
+            />
+          </div>
+        </div>
       </div>
     </Section>
   )
