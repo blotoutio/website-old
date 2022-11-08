b30a6999e4ffdf0f4a73e879d1821ce06c51574d
Yash Ghelani <75333858+yashghelani@users.noreply.github.com>
2022-11-08 14:58:18 +0000

    Update case study banner text

--- app/components/core/LinkBanner/index.jsx
+++ app/components/core/LinkBanner/index.jsx
@@ -4,8 +4,8 @@ import { codifyClick } from '~/utils'
 const LinkBanner = () => {
   return (
     <div className='banner'>
-      <span>New Case Study:</span> How Pura Vida used Blotout to expand their
-      most valuable audience segments.
+      <span>New Case Study:</span> Pura Vida captures returning customers with
+      Blotout Edgetag.
       <a
         href='doc/pura-vida-klaviyo-case-study-by-blotout.pdf'
         download='doc/pura-vida-klaviyo-case-study-by-blotout.pdf'
